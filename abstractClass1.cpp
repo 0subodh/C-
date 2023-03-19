f65802ad1852de7708843fb5c29da65386585582
Subodh Adhikari <subodh@rikesh.rikes>
2023-03-19 19:43:45 +0545

    abstract class example

---
+++ abstractClass1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// a class having at least one pure virtual function is called abstract class
+// an abstract class cannot be instantiated
+// derived class will implement the incomplete functionalites in abstract class
+class Base
+{
+    int x;
+
+public:
+    // fun() is a pure virtual function
+    virtual void fun() = 0;
+    int getX() { return x; }
+};
+
+class Derived : public Base
+{
+    int y;
+
+public:
+    void fun() { cout << "fun() called"; }
+};
+
+int main()
+{
+    Derived d;
+    d.fun();
+    return 0;
+}
